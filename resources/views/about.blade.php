76f38b7ddbd44ebd56b0eb3afc8fa8ca1a6feab5
John <john@Johns-MacBook-Pro.local>
2023-11-12 22:22:16 -0600

    rm link

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -9,7 +9,7 @@
         <h1 class="text-center pt-5 pb-5" >About</h1>
 
         <div class="essay-body">
-            <p><i>Novelglot</i> was created by <a href="https://johngu.io/">John Gu</a>. 
+            <p><i>Novelglot</i> was created by John Gu. 
         </div>
         
     </div>
